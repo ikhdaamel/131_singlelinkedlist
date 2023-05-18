4c3a86d6eaceec92365838de76fec5c6a68dea6b
ikhdaamel <ikhda.new45@gmail.com>
2023-05-18 21:02:35 +0700

    membuat void search data

--- 131_singlelinkedlist/131_singlelinkedlist.cpp
+++ 131_singlelinkedlist/131_singlelinkedlist.cpp
@@ -108,3 +108,26 @@ void traverse() {
 		}
 	}
 }
+
+void searchData() {
+	if (listEmpty()) {
+		cout << "list kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		int nim;
+		cout << "masukan NIM : ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM : " << currentNode->noMhs << ", Nama" << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
+	}
+}
