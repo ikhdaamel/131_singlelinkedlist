597c9792e0ec9102f3aca0eea168c1c72dce1f51
ikhdaamel <ikhda.new45@gmail.com>
2023-05-18 19:58:25 +0700

    void addNode

--- 131_singlelinkedlist/131_singlelinkedlist.cpp
+++ 131_singlelinkedlist/131_singlelinkedlist.cpp
@@ -8,3 +8,15 @@ struct Node {
 }; 
 
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "masukan NIM : ";
+	cin >> nim;
+	cout << "masukan nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+}
