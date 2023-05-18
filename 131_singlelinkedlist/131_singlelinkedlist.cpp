8416fd1c2b44165e8c377e4a1363ece6f141dcb0
ikhdaamel <ikhda.new45@gmail.com>
2023-05-18 20:07:12 +0700

    membuat while

--- 131_singlelinkedlist/131_singlelinkedlist.cpp
+++ 131_singlelinkedlist/131_singlelinkedlist.cpp
@@ -26,5 +26,23 @@ void addNode() {
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
 	}
 }
